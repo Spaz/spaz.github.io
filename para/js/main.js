aa3fa538b487a5989832877844dfca91876ff49b
Spaz <Spaz@PazMacBookPro.local>
2014-05-22 22:32:00 +0300

    connect four and many more

--- para/js/main.js
+++ para/js/main.js
@@ -1,14 +1,32 @@
-var colors = ['#b04040', '#00b080', '#0080b0'];
-var currentColorIndex; // (0, 1, 2) = (r, g, b)
+var colors = ['#00b080', '#b04040', '#0080b0'];
+var currentPlayer = CellEnum.EMPTY; // (0, 1, 2) = (green, red, blue) = (empty, player1, player2)
     
 $(document).ready(function () {
     
-    function cleanFinished() {
-        if (!isBoardActive(board)) {
-            $(".grid-container").animate({
-                opacity: 0.5
-            }, 500 );
+    addGame(GameEnum.TIC_TAC_TOE);
+    addGame(GameEnum.CONNECT_FOUR);
+    
+    function cleanFinished(game) {
+        
+        if (game) {
+            console.log("active = " + game.isBoardActive());
+            if (!game.isBoardActive()) {
+                $("#" + game.id).animate({
+                    opacity: 0.20
+                }, 1000 );
+                if (currentPlayer == CellEnum.PLAYER_ONE) {
+                    playerOneScore++;
+                    $(".player-one-score").text(playerOneScore);
+                } else if (currentPlayer == CellEnum.PLAYER_TWO) {
+                    playerTwoScore++;
+                    $(".player-two-score").text(playerTwoScore);
+                }
+                
+            }
+        } else {
+            console.log("nogame - " + JSON.stringify(game));
         }
+        anime();
     }
     
     $(".grid-cell").click(function () {
@@ -17,32 +35,46 @@ $(document).ready(function () {
         var boardIndex = splitCell[0];
         var row = splitCell[1] - 1;
         var col = splitCell[2] - 1;
-        
-        if (isBoardActive(board)) {
-            console.log("board is active");
-            if (isCellEmpty(board, row, col)) {
-                console.log("cell is empty");
-                console.log("color index is: " + currentColorIndex);
-                if (currentColorIndex == 0) {
-                    board[row][col] = CellEnum.PLAYER_ONE;
+
+        var game = games[boardIndex];
+        if (game.gameType == GameEnum.TIC_TAC_TOE) {
+            console.log("game is tic-tac-toe");
+            if (game.tryPlayMove(currentPlayer, row, col)) {
+                console.log("try play success of player : " + JSON.stringify(currentPlayer));
+                if (currentPlayer == CellEnum.PLAYER_ONE) {
                     $("#"+cellid).addClass("player-one", 1000);
-                    cleanFinished();
-                } else if (currentColorIndex == 2) {
-                    board[row][col] = CellEnum.PLAYER_TWO;
-                    $("#"+cellid).addClass("player-two", 1000 );
-                    cleanFinished();
-                } else {
-                    // WTF ( no player? )
-                    alert("no current player");
+                } else if (currentPlayer == CellEnum.PLAYER_TWO) {
+                    $("#"+cellid).addClass("player-two", 1000);
                 }
-                
-                anime();
+
+                cleanFinished(game);
+            }
+        } else if (game.gameType == GameEnum.CONNECT_FOUR) {
+            console.log("game is connect four");
+            var nextEmptyRow = game.getNextEmptyRowInColumn(row, col);
+            var newCellid = "" + boardIndex + "-" + (nextEmptyRow+1) + "-" + (col+1);
+
+            if (game.tryPlayMove(currentPlayer, row, col)) {
+                console.log("last: " + game.lastMoveRow);
+                var newCellid = "" + boardIndex + "-" + (game.lastMoveRow+1) + "-" + (col+1);
+                if (currentPlayer == CellEnum.PLAYER_ONE) {
+                    $("#"+newCellid).addClass("player-one", 1000);
+                } else if (currentPlayer == CellEnum.PLAYER_TWO) {
+                    $("#"+newCellid).addClass("player-two", 1000);
+                }
+                cleanFinished(game);
+            } else {
+                console.log("did not play move");
             }
         }
+        
+        
+
+        console.log("click ended, current player is: " + currentPlayer);
     });
-    
+        
     function anime() {
-        var i = 2 - currentColorIndex; // 2 to 0, 1 to 1, 0 to 2
+        var i = 3 - currentPlayer; // 2 to 0, 1 to 1, 0 to 2 (empty stays empty, players switch)
         $('body:before').animate({
             opacity: 0.5
         }, 500 );
@@ -52,10 +84,12 @@ $(document).ready(function () {
         $('body:before').animate({
             opacity: 1
         }, 500 );
-        currentColorIndex = i;
+        currentPlayer = i;
     }
     
-    currentColorIndex = Math.floor(2 * Math.random()) == 0 ? 0 : 2;
+    currentPlayer = Math.floor(1 + 2 * Math.random());
+    console.log("current player was randomly selected to be: " + currentPlayer);
+    
     anime();
 });
 
--- para/js/tictactoe.js
+++ para/js/tictactoe.js
@@ -1,74 +1,102 @@
-var size = 3;
-var CellEnum = {
-    EMPTY : 0,
-    PLAYER_ONE : 1,
-    PLAYER_TWO : 2
-};
-Object.freeze(CellEnum);
-var currentPlayer = CellEnum.EMPTY;
+function ticTacToe(id) {
+    var ttt = {
 
-var board = [
-    [CellEnum.EMPTY, CellEnum.EMPTY, CellEnum.EMPTY],
-    [CellEnum.EMPTY, CellEnum.EMPTY, CellEnum.EMPTY],
-    [CellEnum.EMPTY, CellEnum.EMPTY, CellEnum.EMPTY]
-];
-
-function isCellEmpty(array, row, col) {
-    console.log("board["+row+"]["+col+"] = " + array[row][col]); 
-    return array[row][col] == CellEnum.EMPTY;
-}
-
-function isCurrentTurn(player) {
-    return currentPlayer == player;
-}
+        id : id,
         
-function getRowWinner(array, row) {
-    if (array[row][0] == array[row][1] && array[row][0] == array[row][2]) {
-        return array[row][0];
-    }
-    
-    return CellEnum.EMPTY;
-}
+        gameType : GameEnum.TIC_TAC_TOE,
+        
+        size : 3,
 
-function getColWinner(array, col) {
-    if (array[0][col] == array[1][col] && array[0][col] == array[2][col]) {
-        return array[0][col];
-    }
-    
-    return CellEnum.EMPTY;
-}
+        currentPlayer : CellEnum.EMPTY,
 
-function getDiagWinner(array) {
-    if (array[0][0] == array[1][1] && array[0][0] == array[2][2]) {
-        return array[0][0];
-    } else if (array[0][2] == array[1][1] && array[0][2] == array[2][0]) {
-        return array[0][2];
-    }
-                     
-    return CellEnum.EMPTY;
-}
+        board : [
+            [CellEnum.EMPTY, CellEnum.EMPTY, CellEnum.EMPTY],
+            [CellEnum.EMPTY, CellEnum.EMPTY, CellEnum.EMPTY],
+            [CellEnum.EMPTY, CellEnum.EMPTY, CellEnum.EMPTY]
+        ],
 
-function getWinner(array) {
-    var winner = getDiagWinner(array), i;
-    if (winner !== CellEnum.EMPTY) {
-        return winner; // diagonal winner
-    } else {
-        for (i = 0; i < size; i += 1) {
-            winner = getRowWinner(array, i);
-            if (winner !== CellEnum.EMPTY) {
-                return winner; // row winner
+        isCellEmpty : function(row, col) {
+            return this.board[row][col] == CellEnum.EMPTY;
+        },
+
+        isCurrentTurn : function(player) {
+            return currentPlayer == player;
+        },
+        
+        tryPlayMove : function(player, toRow, toCol) {
+            
+            if (player == CellEnum.PLAYER_ONE || player == CellEnum.PLAYER_TWO) {
+                if (this.isBoardActive()) {
+                    if (this.isCellEmpty(toRow, toCol)) {
+                        this.board[toRow][toCol] = player;
+                        return true;
+                    } else {
+                        console.log("cell is not empty");
+                    }
+                } else {
+                    console.log("can't play on a non-active board");
+                }
+            } else if (player == CellEnum.EMPTY) {
+                console.log("playMove got empty player...");
+            }
+            else {
+                console.log("playMove player was" + player);
+            }
+            
+            return false;
+        },
+
+        getRowWinner : function(row) {
+            if (this.board[row][0] == this.board[row][1] && this.board[row][0] == this.board[row][2]) {
+                return this.board[row][0];
+            }
+
+            return CellEnum.EMPTY;
+        },
+
+        getColWinner : function(col) {
+            if (this.board[0][col] == this.board[1][col] && this.board[0][col] == this.board[2][col]) {
+                return this.board[0][col];
+            }
+
+            return CellEnum.EMPTY;
+        },
+
+        getDiagWinner : function() {
+            if (this.board[0][0] == this.board[1][1] && this.board[0][0] == this.board[2][2]) {
+                return this.board[0][0];
+            } else if (this.board[0][2] == this.board[1][1] && this.board[0][2] == this.board[2][0]) {
+                return this.board[0][2];
+            }
+
+            return CellEnum.EMPTY;
+        },
+
+        getWinner : function() {
+            var winner = this.getDiagWinner(), i;
+            if (winner != CellEnum.EMPTY) {
+                return winner; // diagonal winner
             } else {
-                winner = getColWinner(array, i);
-                if (winner !== CellEnum.EMPTY) {
-                    return winner; // column winner
+                for (i = 0; i < this.size; i += 1) {
+                    winner = this.getRowWinner(i);
+                    if (winner != CellEnum.EMPTY) {
+                        return winner; // row winner
+                    } else {
+                        winner = this.getColWinner(i);
+                        if (winner != CellEnum.EMPTY) {
+                            return winner; // column winner
+                        }
+                    }
                 }
             }
+
+            return CellEnum.EMPTY; // no winner
+        },
+
+        isBoardActive : function() {
+            return this.getWinner(this.board) == CellEnum.EMPTY;
         }
     }
-    
-    return CellEnum.EMPTY; // no winner
-}
 
-function isBoardActive(array) {
-    return getWinner(array) == CellEnum.EMPTY;
+    return ttt;
 }
